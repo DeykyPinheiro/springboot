7502066daa3d8ea9b8d6f83814c8992d2d8b0cd1
Deyky Pinheiro <pinheiromikael96@gmail.com>
2022-01-20 13:49:59 -0300

    feat: added Tema

--- blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -6,15 +6,19 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Size;
 
+import org.hibernate.annotations.ManyToAny;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull;
 
 @Entity
-@Table
+@Table(name = "tb_postagem")
 public class Postagem {
 	
 	@Id
@@ -22,15 +26,19 @@ public class Postagem {
 	private long id;
 	
 	@NotNull
-	@Size(min = 10, max = 70)
+	@Size(min = 5, max = 70)
 	private String titulo;
 	
 	@NotNull
-	@Size(min = 10, max = 50)
+	@Size(min = 5, max = 50)
 	private String texto;
 	
 	@Temporal(TemporalType.TIMESTAMP)
     private Date data = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
 	public long getId() {
 		return id;
@@ -62,6 +70,14 @@ public class Postagem {
 
 	public void setData(Date data) {
 		this.data = data;
+	}
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
 	}
 	
 	
