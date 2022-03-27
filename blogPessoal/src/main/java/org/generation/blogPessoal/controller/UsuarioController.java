7bb8250e67f5bba02af865ec71443fc8d7769ee9
Deyky Pinheiro <pinheiromikael96@gmail.com>
2022-03-27 13:16:37 -0300

    fix: error on route

--- blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
@@ -60,7 +60,7 @@ public class UsuarioController {
 			.orElse(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
 	}
 	
-	@PutMapping("/atualiz ar")
+	@PutMapping("/atualizar")
 	public ResponseEntity<Usuario> putUsuario(@Valid @RequestBody Usuario usuario){
 		
 		return usuarioService.atualizarUsuario(usuario)
